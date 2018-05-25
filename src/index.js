52a85f598685a60e2f4d7d469c3852cfac53915a
Alejandro Ñáñez Ortiz <alejonanez@gmail.com>
2018-05-25 12:41:05 -0500

    base styling (#2)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ThemeProvider } from 'styled-components';
+import theme from 'theme';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
---
+++ src/theme/index.js
@@ -0,0 +1,16 @@
+import { css } from 'styled-components';
+
+export const fonts = css`-apple-system, BlinkMacSystemFont, Helvetica, Arial, sans-serif`;
+
+const pallete = {
+  alabaster: '#fafafa',
+  dustyGray: '#999',
+  gallery: '#efefef',
+  mineShaft: '#262626',
+  silver: '#c7c7c7',
+  white: '#fff',
+};
+
+export default {
+  pallete,
+};
